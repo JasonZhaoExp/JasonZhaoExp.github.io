8b902d4defeca58ed5e0b32288d4b84fb2633a68
Jason <110392848+JasonZhaoExp@users.noreply.github.com>
2025-01-09 11:34:59 +0000

    Deploying to GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/JasonZhaoExp.github.io/',
   server: {
     port: 12345,
     host: '127.0.0.1'

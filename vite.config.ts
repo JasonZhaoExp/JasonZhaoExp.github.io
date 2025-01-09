4074e8851a2767fa26a1037fea75fe1d19170fdf
Jason <110392848+JasonZhaoExp@users.noreply.github.com>
2025-01-09 12:00:37 +0000

    retry AGAIN

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    port: 12345,
-    host: '127.0.0.1'
-  }
+  base: '',
 });
